c35df0025052cf3fded9f31d07568c7db5065f32
Yuliia Shvets <y.shvets85@gmail.com>
2025-10-19 19:18:40 +0200

    new last commit

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,20 +1,16 @@
-'use client';
+'use client'
+import { useRouter } from 'next/navigation'
+import PromotionFormModal from '@/app/components/promotion-form-modal'
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import PromotionFormModal from '@/app/components/promotion-form-modal';
-
-export interface PageProps {
-  params: { id: string };
-}
-
-export default function Page({ params }: PageProps) {
-  const router = useRouter();
+export default function Page({ params }: { params: { id: string } }) {
+  const router = useRouter()
   return (
     <PromotionFormModal
       companyId={params.id}
-      show={true}
+      show
       onClose={() => router.back()}
     />
-  );
+  )
 }
+
+
